84cde9e5fc2c02c6de4027a354abb45eba868c18
TobiasHFU <tobias.bobek@hs-furtwangen.de>
2025-03-29 21:40:14 +0100

    update

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     showRabattCall(); // Starte mit RabattCall
 
-    // Slideshow Steuerung
+     // Slideshow Steuerung
     let slideIndex = 0;
     const slides = document.querySelectorAll('.slide');
     const dots = document.querySelectorAll('.dot');
